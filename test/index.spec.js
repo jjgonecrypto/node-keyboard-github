56495ee9fdf6afdda010d57c87fecfe96c0a8e1b
justin j. moses <justinjmoses@gmail.com>
2017-05-20 18:53:40 -0400

    Removing redudant files

--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-
-const  = require('../')
-
-describe('node-keyboard-github', function() {
-    it('is truthy', () => {
-        assert(, ' must be truthy')
-    })
-})
